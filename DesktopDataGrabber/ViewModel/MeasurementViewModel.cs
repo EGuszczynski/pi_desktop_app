ce648e6e124851255a4f61809832d356823d82e9
Emil Guszczyński <emil.guszczynski@gmail.com>
2020-09-23 23:50:26 +0200

    Updating missing file

---
+++ DesktopDataGrabber/ViewModel/MeasurementViewModel.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopDataGrabber.ViewModel
+{
+    using Model;
+
+    public class MeasurementViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        private double _value;
+        public string Value
+        {
+            get
+            {
+                return _value.ToString("N1", CultureInfo.InvariantCulture);
+            }
+            set
+            {
+                if (Double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out double tmp) && _value != tmp)
+                {
+                    _value = tmp;
+                    OnPropertyChanged("Value");
+                }
+            }
+        }
+
+        private string _unit;
+        public string Unit
+        {
+            get
+            {
+                return _unit;
+            }
+            set
+            {
+                if (_unit != value)
+                {
+                    _unit = value;
+                    OnPropertyChanged("Unit");
+                }
+            }
+        }
+
+        public MeasurementViewModel(MeasurementModel model)
+        {
+            UpdateWithModel(model);
+        }
+
+        public void UpdateWithModel(MeasurementModel model)
+        {
+            _name = model.Name;
+            OnPropertyChanged("Name");
+            if (model.Unit == "deg" && model.Value > 180)
+            {
+                _value = model.Value - 360;
+            }
+            else
+            {
+                _value = model.Value;
+            }
+            OnPropertyChanged("Value");
+            _unit = model.Unit;
+            OnPropertyChanged("Unit");
+        }
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /**
+         * @brief Simple function to trigger event handler
+         * @params propertyName Name of ViewModel property as string
+         */
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
